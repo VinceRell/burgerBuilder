389a82889f532ee7657f6566a2ddec87bc6fe3ab
Vincent <v.rell_home@outlook.com>
2017-12-15 05:48:04 +0100

    added toolbar and navigation buttons

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-//components
 import Aux from "../../hoc/Auxiliary";
-
+import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const Layout = (props) =>(
   <Aux>
-    <div>Toolbar, SideDrawer</div>
-    <main>
+    <Toolbar/>
+    <SideDrawer />
+    <main className={classes.Content}>
         {props.children}
     </main>
   </Aux>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Toolbar = ()=> (
+import classes from "./Toolbar.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems"
 
+
+const Toolbar = (props)=> (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <Logo />
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default Toolbar;
