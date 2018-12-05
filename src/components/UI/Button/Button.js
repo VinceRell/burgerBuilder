3bc37f9b8214b0503856d1dc3e3c0c8d420d4a79
Vincent <v.rell_home@outlook.com>
2018-12-05 01:42:16 +0100

    finished styling form and functionality

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,7 +3,8 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const button = (props) => (
-    <button 
+    <button
+        disabled={props.disabled} 
         className={[classes.Button, classes[props.BtnType]].join(" ")}
         onClick={props.clicked}>
         {props.children}
